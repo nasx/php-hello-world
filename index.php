bdf1c0229c7bfc5249d7cb26f73049c5726d1b52
Chris Keller <ckeller@redhat.com>
2019-01-07 13:31:00 -0500

    Initial Commit.

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Hello World!</title>
+</head>
+
+<body>
+ 
+  <h1>Hello World!</h1>
+  <pre>Connected to <?php echo $_SERVER; ?></pre>
+
+</body>
+<html>
+
+<!-- comment -->
